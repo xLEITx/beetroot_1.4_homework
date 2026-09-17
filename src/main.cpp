e8a64bbc57b37d18a74e68e53406c2c5fe1f2eee
Vladyslav Kyrychok <vladyslav.kyrychokv@gmail.com>
2026-09-17 18:22:08 +0300

    add blinking modes

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,36 @@
 #define BOARD_BUTTON 0
 #define OUTER_BUTTON 21
 
+enum Mode{
+  SYNC,
+  ASYNC
+};
+
+int mode = SYNC;
+
 void setup() {
   pinMode(LED_RED, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(BOARD_BUTTON, INPUT);
+  pinMode(OUTER_BUTTON, INPUT_PULLUP);
 }
 
 void loop() {
+
+  if(mode == SYNC){
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+    delay(200);
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_BLUE, LOW);
+    delay(200);
+  }else if(mode == ASYNC){
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_BLUE, LOW);
+    delay(1000);
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_BLUE, HIGH);
+    delay(1000);
+  }
   
 }
